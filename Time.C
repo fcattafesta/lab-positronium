287575fabd2bcc649423d4a1e7b463033632dc87
fcattafesta <f.cattafesta@studenti.unipi.it>
2022-05-25 16:03:16 +0200

    TripleCut analysis

--- Time.C
+++ Time.C
@@ -6,7 +6,7 @@ void Time() {
 
   double time_1, time_2, time_3;
   int nbins = 50;
-  double inf = -100, sup = 100;
+  double inf = -1000, sup = 1000;
 
   auto f1 = new TFile(treepath[0].c_str(), "READ");
   auto t1 = f1->Get<TTree>("tree;3");
@@ -24,26 +24,38 @@ void Time() {
 
   auto h_21 = new TH1D("h_21", ";#DeltaT_{21} [ns]", nbins, inf, sup);
   auto h_31 = new TH1D("h_31", ";#DeltaT_{31} [ns]", nbins, inf, sup);
+  auto h_32 = new TH1D("h_32", ";#DeltaT_{32} [ns]", nbins, inf, sup);
 
   auto g = new TGraph();
+  auto g1 = new TGraph();
 
   for (int i=0; i<nentries; i++) {
     t1->GetEntry(i); t2->GetEntry(i); t3->GetEntry(i);
     auto delta_21 = time_2 - time_1;
     auto delta_31 = time_3 - time_1;
-    if (delta_31 >= inf && delta_31 <= sup && delta_21 >= inf && delta_21 <= sup)
+    auto delta_32 = time_3 - time_2;
+    if (delta_31 >= inf && delta_31 <= sup && delta_21 >= inf && delta_21 <= sup &&
+        delta_32 >= inf && delta_32 <= sup) {
     g->AddPoint(delta_21, delta_31);
+    g1->AddPoint(delta_21, delta_32);
+  }
     if (delta_21 >= inf && delta_21 <= sup) h_21->Fill(delta_21);
     if (delta_31 >= inf && delta_31 <= sup) h_31->Fill(delta_31);
+    if (delta_32 >= inf && delta_32 <= sup) h_32->Fill(delta_32);
+
   }
 
   auto c1 = new TCanvas();
   h_21->Draw();
-  c1->SaveAs("figures/triple/time_21.pdf");
+  //c1->SaveAs("figures/triple/time_21.pdf");
 
   auto c2 = new TCanvas();
   h_31->Draw();
-  c2->SaveAs("figures/triple/time_31.pdf");
+  //c2->SaveAs("figures/triple/time_31.pdf");
+
+  auto c0 = new TCanvas();
+  h_32->Draw();
+  //c0->SaveAs("figures/triple/time_32.pdf");
 
   auto c3 = new TCanvas(); c3->SetGrid();
   g->Draw("AP");
@@ -52,5 +64,14 @@ void Time() {
   xaxis->SetLimits(inf, sup);
   auto yaxis = g->GetYaxis(); yaxis->SetTitle("#Deltat_{31} [ns]");
   yaxis->SetRangeUser(inf, sup);
-  c3->SaveAs("figures/triple/21vs31.pdf");
+  //c3->SaveAs("figures/triple/21vs31.pdf");
+
+  auto c4 = new TCanvas(); c4->SetGrid();
+  g1->Draw("AP");
+  g1->SetMarkerStyle(8);
+  auto xaxis1 = g1->GetXaxis(); xaxis1->SetTitle("#Deltat_{21} [ns]");
+  xaxis1->SetLimits(inf, sup);
+  auto yaxis1 = g1->GetYaxis(); yaxis1->SetTitle("#Deltat_{32} [ns]");
+  yaxis1->SetRangeUser(inf, sup);
+  //c4->SaveAs("figures/triple/21vs32.pdf");
 }
--- TripleCut.C
+++ TripleCut.C
@@ -1,26 +1,25 @@
-int Cut(double Energy, double UpLim, double LowLim) {
+int Cut(double data, double inf, double sup) {
 
-  if (Energy < LowLim || Energy > UpLim) return -1;
+  if (data < inf || data > sup) return -1;
   return 1;
 }
+
 void TripleCut() {
 
   std::string treepath[3] = {"data/2405/triple_1.root",
                              "data/2405/triple_2.root",
-                             "data/2405/triple_3.root"},
-              figpath = "figures/triple/1705_triple_pol%dfix_no_cut.pdf",
-              elementname = "{}^{22}Na";
+                             "data/2405/triple_3.root"};
 
-  double a1 = 42.3, a2 = 206.7, a3 = 86.9 ,
-         da1 = 0.2, da2 = 2.5, da3 = 1.2;
+  double a1 = 40, a2 = 223.38, a3 = 88.85 ,
+         da1 = 0.2, da2 = 8.62, da3 = 3.56;
   double b1 = 0, b2 = 0, b3 = 0;
 
   double data1, data2, data3, Energy1, Energy2, Energy3, time_1, time_2, time_3;
 
   int nbins = 50;
   double inf = 0, sup = 2e3;
-  double EnergyLow = inf,
-         EnergyUp = sup;
+  double EnergyLow = 1e2,
+         EnergyUp = 4.5e2;
 
 
   auto f1 = new TFile(treepath[0].c_str(), "READ");
@@ -42,7 +41,22 @@ void TripleCut() {
 
   int nentries = t1->GetEntries();
 
-  TH1D * h = new TH1D("h", "; Energy [keV]", nbins, inf, sup);
+  TH1D * h = new TH1D("h", "Total Energy (coincidence); Energy [keV]", nbins, inf, sup);
+  TH1D * h_t = new TH1D("h_t", "Total Energy (out-of-coincidence); Energy [keV]",
+                        nbins, inf, sup);
+
+  TH1D * h_1 = new TH1D("h_1", "pmt1 (coincidence); Energy [keV]", nbins, inf, sup);
+  TH1D * h_t1 = new TH1D("h_t1", "pmt1 (out-of-coincidence); Energy [keV]",
+                         nbins, inf, sup);
+
+  TH1D * h_2 = new TH1D("h_2", "pmt2 (coincidence); Energy [keV]", nbins, inf, sup);
+  TH1D * h_t2 = new TH1D("h_t2", "pmt2 (out-of-coincidence); Energy [keV]",
+                         nbins, inf, sup);
+
+  TH1D * h_3 = new TH1D("h_3", "pmt3 (coincidence); Energy [keV]", nbins, inf, sup);
+  TH1D * h_t3 = new TH1D("h_t3", "pmt3 (out-of-coincidence); Energy [keV]",
+                         nbins, inf, sup);
+
 
     for (int i=0; i<nentries; i++) {
       t1->GetEntry(i); t2->GetEntry(i); t3->GetEntry(i);
@@ -51,12 +65,56 @@ void TripleCut() {
       data3 = (Energy3 - b3) / a3;
       auto time_21 = time_2 - time_1;
       auto time_31 = time_3 - time_1;
-      if ((Cut(data1+data2+data3, EnergyUp, EnergyLow) == 1) &&
-           abs(time_21) <= 20 && abs(time_31) <= 20) {
-             h->Fill(data1+data2+data3);
-           }
+      auto time_32 = time_3 - time_2;
+
+      if (Cut(time_21, -30, 20) == 1 && Cut(time_31, -40, 20) == 1 &&
+          Cut(time_32, -20, 25) == 1) {
+
+            if (Cut(data1, inf, sup) == 1) h_1->Fill(data1);
+
+            if (Cut(data2, inf, sup) == 1) h_2->Fill(data2);
+
+            if (Cut(data3, inf, sup) == 1) h_3->Fill(data3);
+
+            if (Cut(data1, EnergyLow, EnergyUp) == 1 &&
+                Cut(data2, EnergyLow, EnergyUp) == 1 &&
+                Cut(data3, EnergyLow, EnergyUp) == 1)
+                h->Fill(data1 + data2 + data3);
+      }
+      if (Cut(time_21, -30, 20) == -1 && Cut(time_31, -40, 20) == -1 &&
+          Cut(time_32, -20, 25) == -1) {
+
+            if (Cut(data1, inf, sup) == 1) h_t1->Fill(data1);
+
+            if (Cut(data2, inf, sup) == 1) h_t2->Fill(data2);
+
+            if (Cut(data3, inf, sup) == 1 ) h_t3->Fill(data3);
+
+            if (Cut(data1, EnergyLow, EnergyUp) == 1 &&
+                Cut(data2, EnergyLow, EnergyUp) == 1 &&
+                Cut(data3, EnergyLow, EnergyUp) == 1)
+                h_t->Fill(data1 + data2 + data3);
+      }
     }
 
+    auto c = new TCanvas();
     h->Draw();
+    auto c_t = new TCanvas();
+    h_t->Draw();
+
+    auto c1 = new TCanvas();
+    h_1->Draw();
+    auto c_t1 = new TCanvas();
+    h_t1->Draw();
+
+    auto c2 = new TCanvas();
+    h_2->Draw();
+    auto c_t2 = new TCanvas();
+    h_t2->Draw();
+
+    auto c3 = new TCanvas();
+    h_3->Draw();
+    auto c_t3 = new TCanvas();
+    h_t3->Draw();
 
 }
